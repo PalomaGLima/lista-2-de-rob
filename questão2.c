f8ed6291fba51b4b0e8e355f43d0d373c634902c
Paloma <palomalima272@gmail.com>
2023-04-24 18:20:45 -0300

    07

--- questão2.c
+++ questão2.c
@@ -1,4 +1,4 @@
-/*02. Faça um programa que receba dois números. Calcule a soma dos dois números. Caso a 
+/*02- Faça um programa que receba dois números. Calcule a soma dos dois números. Caso a 
 soma seja maior que 10, imprima o resultado da soma.*/
 
 #include <stdio.h>
