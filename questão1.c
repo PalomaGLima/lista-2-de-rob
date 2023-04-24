a9b5f84aefb9e9818d71d150cb7315eae460640a
Paloma <palomalima272@gmail.com>
2023-04-24 18:20:11 -0300

    06

--- questão1.c
+++ questão1.c
@@ -1,4 +1,4 @@
-/*01. Faça um programa que receba um número e imprima ele caso seja maior que 20.*/
+/*01- Faça um programa que receba um número e imprima ele caso seja maior que 20.*/
 
 #include <stdio.h>
 #include <stdlib.h>
